83ed150a01cf6173148e0bf9e6bb28e52f36621c
SDDLOL19 <santidiazd@gmail.com>
2023-03-19 18:59:01 +0100

    Inicio del AnimationSystem
    Lo que dice el título

--- Assets/Scripts/Jugador.cs
+++ Assets/Scripts/Jugador.cs
@@ -13,10 +13,7 @@ public class Jugador : MonoBehaviour
     //private enum Estado { Idle, Move, Saltar, Caer, Dash }
 
     public bool Dash;
-    public float Dash_T;
-
-    public bool SaltarAni;
-    public bool CaidaAni;
+    public float Dash_Time;
 
     private void Start()
     {
@@ -29,29 +26,46 @@ public class Jugador : MonoBehaviour
         Movimiento();
         Salto();
         DashMove();
-        AnimacionSalto();
-
-
+        AnimationSystem();
     }
+
+    public void AnimationSystem()
+    {
+        if (!Dash)
+        {
+            if (CR.velocity.x == 0)
+            {
+                anim.SetBool("moving", false);
+            }
+
+            if (CR.velocity.x != 0)
+            {
+                anim.SetBool("moving", true); //Establecerle la animcaion de caminar
+            }
+        }
+
+        if (CR.velocity.y > 0)
+        {
+            gameObject.GetComponent<Animator>().SetBool("saltar", true);
+        }
+
+        if (CR.velocity.y < 0)
+        {
+            gameObject.GetComponent<Animator>().SetBool("caer", true);
+        }
+    }
+
     public void Movimiento()
     {
         if (Keyboard.current.leftArrowKey.isPressed && !Dash)
         {
             CR.AddForce(new Vector2(-1000f * Time.deltaTime, 0));
-            anim.SetBool("moving", true); //Establecerle la animcaion de caminar
             gameObject.GetComponent<SpriteRenderer>().flipX = true; //para dar la vuelta al personaje
         }
         if (Keyboard.current.rightArrowKey.isPressed && !Dash)
         {
             CR.AddForce(new Vector2(1000f * Time.deltaTime, 0));
-            anim.SetBool("moving", true);
             gameObject.GetComponent<SpriteRenderer>().flipX = false;
-        }
-
-        if(CR.velocity.x != 0)
-        {
-            anim.SetBool("moving", false);
-
         }
     }
     public void Salto()
@@ -63,26 +77,13 @@ public class Jugador : MonoBehaviour
 
         }
     }
-    public void AnimacionSalto()
-    {
-        if (CR.velocity.y < 0.1)
-        {
-            SaltarAni = true;
-            gameObject.GetComponent<Animator>().SetBool("saltar", true);
-        }
-        if (CR.velocity.y > -0.1)
-        {
-            CaidaAni = true;
-            gameObject.GetComponent<Animator>().SetBool("caer", true);
-        }
-    }
 
     public void DashMove()
     {
         if (Keyboard.current.spaceKey.isPressed)
         {
-            Dash_T += 1 * Time.deltaTime;
-            if (Dash_T < 0.35f && Keyboard.current.rightArrowKey.isPressed && !Keyboard.current.leftArrowKey.isPressed)
+            Dash_Time += 1 * Time.deltaTime;
+            if (Dash_Time < 0.35f && Keyboard.current.rightArrowKey.isPressed && !Keyboard.current.leftArrowKey.isPressed)
             {
 
                 Dash = true;
@@ -90,7 +91,7 @@ public class Jugador : MonoBehaviour
                 transform.Translate(Vector3.right * 300f * Time.deltaTime);
                 Shadows.me.Sombras_skill();
             }
-            if (Dash_T < 0.35f && Keyboard.current.leftArrowKey.isPressed && !Keyboard.current.rightArrowKey.isPressed)
+            if (Dash_Time < 0.35f && Keyboard.current.leftArrowKey.isPressed && !Keyboard.current.rightArrowKey.isPressed)
             {
                 Dash = true;
                 anim.SetBool("dash", true);
@@ -108,7 +109,7 @@ public class Jugador : MonoBehaviour
         {
             Dash = false;
             anim.SetBool("dash", false);
-            Dash_T = 0;
+            Dash_Time = 0;
         }
     }
 
