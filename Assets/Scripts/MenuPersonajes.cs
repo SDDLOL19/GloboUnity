e966a2fca726c0d2588a0a4f757f50f0a819a8d8
SDDLOL19 <santidiazd@gmail.com>
2023-05-06 00:11:46 +0200

    MenúsFuncionandoCorrectamente

--- Assets/Scripts/MenuPersonajes.cs
+++ Assets/Scripts/MenuPersonajes.cs
@@ -2,38 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class MenuPersonajes : MonoBehaviour
 {
     [SerializeField] Sprite[] spritesPersonajes;
     [SerializeField] SpriteRenderer[] spritesJugadores;
-    [SerializeField] GameObject[] posicionOpciones;
+    [SerializeField] GameObject[] posicionPersonajes;
     [SerializeField] GameObject[] posicionEscenarios;
     [SerializeField] GameObject[] rebordes;
 
-    bool PersonajeUnoSeleccionado;
-    bool PersonajeDosSeleccionado;
-    bool EscenarioSeleccionado;
-
     private void Awake()
     {
-        SpritesManager.personajeJugadorUno = 1;
-        SpritesManager.personajeJugadorDos = 1;
-        SpritesManager.fondoEscenario = 1;
-        PersonajeUnoSeleccionado = false;
-        PersonajeDosSeleccionado = false;
-        EscenarioSeleccionado = false;
+        SpritesManager.personajeJugadorUno = 0;
+        SpritesManager.personajeJugadorDos = 0;
+        SpritesManager.fondoEscenario = 0;
+    }
+
+    private void Start()
+    {
+        ActualizarPersonajes();
     }
 
     void Update()
     {
-        PosicionRebordes();
         ControlesOpcionesPersonajes();
-        LimitarVariables();
+        ControlesOpcionesFondo();        
+        EmpezarPartida();
 
+        if (Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            SceneManager.LoadScene(0);
+        }
     }
 
-    void LimitarVariables()
+    void ActualizarPersonajes()
+    {
+        spritesJugadores[0].sprite = spritesPersonajes[SpritesManager.personajeJugadorUno];
+        spritesJugadores[1].sprite = spritesPersonajes[SpritesManager.personajeJugadorDos];
+    }
+
+    void EmpezarPartida()
+    {
+        if (SpritesManager.personajeJugadorUno != 0 && SpritesManager.personajeJugadorDos != 0 && SpritesManager.fondoEscenario != 0)
+        {
+            if (Keyboard.current.enterKey.wasPressedThisFrame)
+            {
+                SceneManager.LoadScene(2);
+            }
+        }
+    }
+
+    void LimitarVariablesJugadorUno()
     {
         //Límites jugador 1
         if (SpritesManager.personajeJugadorUno < 0)
@@ -45,7 +65,10 @@ public class MenuPersonajes : MonoBehaviour
         {
             SpritesManager.personajeJugadorUno = (spritesPersonajes.Length - 1);
         }
+    }
 
+    void LimitarVariablesJugadorDos()
+    {
         //Límites jugador 2
         if (SpritesManager.personajeJugadorDos < 0)
         {
@@ -56,7 +79,10 @@ public class MenuPersonajes : MonoBehaviour
         {
             SpritesManager.personajeJugadorDos = (spritesPersonajes.Length - 1);
         }
+    }
 
+    void LimitarVariablesFondo()
+    {
         //Límites fondos
         if (SpritesManager.fondoEscenario < 0)
         {
@@ -75,54 +101,105 @@ public class MenuPersonajes : MonoBehaviour
         if (Keyboard.current.wKey.wasPressedThisFrame)
         {
             SpritesManager.personajeJugadorUno -= 3;
+            LimitarVariablesJugadorUno();
+            ColocarRebordesJugadorUno();
+            ActualizarPersonajes();
         }
 
         if (Keyboard.current.sKey.wasPressedThisFrame)
         {
             SpritesManager.personajeJugadorUno += 3;
+            LimitarVariablesJugadorUno();
+            ColocarRebordesJugadorUno();
+            ActualizarPersonajes();
         }
 
         if (Keyboard.current.aKey.wasPressedThisFrame)
         {
             SpritesManager.personajeJugadorUno--;
+            LimitarVariablesJugadorUno();
+            ColocarRebordesJugadorUno();
+            ActualizarPersonajes();
         }
 
         if (Keyboard.current.dKey.wasPressedThisFrame)
         {
             SpritesManager.personajeJugadorUno++;
+            LimitarVariablesJugadorUno();
+            ColocarRebordesJugadorUno();
+            ActualizarPersonajes();
         }
 
         //Controles jugador 2
-        if (Keyboard.current.wKey.wasPressedThisFrame)
+        if (Keyboard.current.upArrowKey.wasPressedThisFrame)
         {
             SpritesManager.personajeJugadorDos -= 3;
+            LimitarVariablesJugadorDos();
+            ColocarRebordesJugadorDos();
+            ActualizarPersonajes();
         }
 
-        if (Keyboard.current.sKey.wasPressedThisFrame)
+        if (Keyboard.current.downArrowKey.wasPressedThisFrame)
         {
             SpritesManager.personajeJugadorDos += 3;
+            LimitarVariablesJugadorDos();
+            ColocarRebordesJugadorDos();
+            ActualizarPersonajes();
         }
 
-        if (Keyboard.current.aKey.wasPressedThisFrame)
+        if (Keyboard.current.leftArrowKey.wasPressedThisFrame)
         {
             SpritesManager.personajeJugadorDos--;
+            LimitarVariablesJugadorDos();
+            ColocarRebordesJugadorDos();
+            ActualizarPersonajes();
         }
 
-        if (Keyboard.current.dKey.wasPressedThisFrame)
+        if (Keyboard.current.rightArrowKey.wasPressedThisFrame)
         {
             SpritesManager.personajeJugadorDos++;
+            LimitarVariablesJugadorDos();
+            ColocarRebordesJugadorDos();
+            ActualizarPersonajes();
         }
     }
 
     void ControlesOpcionesFondo()
     {
+        if (Keyboard.current.digit1Key.wasPressedThisFrame || Keyboard.current.numpad1Key.wasPressedThisFrame)
+        {
+            SpritesManager.fondoEscenario = 1;
+            LimitarVariablesFondo();
+            ColocarRebordesFondo();
+        }
 
+        if (Keyboard.current.digit2Key.wasPressedThisFrame || Keyboard.current.numpad2Key.wasPressedThisFrame)
+        {
+            SpritesManager.fondoEscenario = 2;
+            LimitarVariablesFondo();
+            ColocarRebordesFondo();
+        }
+
+        if (Keyboard.current.digit3Key.wasPressedThisFrame || Keyboard.current.numpad3Key.wasPressedThisFrame)
+        {
+            SpritesManager.fondoEscenario = 3;
+            LimitarVariablesFondo();
+            ColocarRebordesFondo();
+        }
     }
 
-    void PosicionRebordes()
+    void ColocarRebordesJugadorUno()
     {
-        rebordes[0].transform.position = posicionOpciones[SpritesManager.personajeJugadorUno].transform.position;
-        rebordes[1].transform.position = posicionOpciones[SpritesManager.personajeJugadorDos].transform.position;
-        rebordes[2].transform.position = posicionOpciones[SpritesManager.fondoEscenario].transform.position;
+        rebordes[0].transform.position = posicionPersonajes[SpritesManager.personajeJugadorUno].transform.position;
+    }
+
+    void ColocarRebordesJugadorDos()
+    {
+        rebordes[1].transform.position = posicionPersonajes[SpritesManager.personajeJugadorDos].transform.position;
+    }
+
+    void ColocarRebordesFondo()
+    {
+        rebordes[2].transform.position = posicionEscenarios[SpritesManager.fondoEscenario].transform.position;
     }
 }
