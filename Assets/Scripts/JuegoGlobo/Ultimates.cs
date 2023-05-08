84ef4e7f9a732b8959f3c3ea7216728d439dd126
SDDLOL19 <santidiazd@gmail.com>
2023-05-08 17:11:38 +0200

    Menú y pantalla de carga

--- Assets/Scripts/JuegoGlobo/Ultimates.cs
+++ Assets/Scripts/JuegoGlobo/Ultimates.cs
@@ -4,16 +4,46 @@ using UnityEngine;
 
 public class Ultimates : MonoBehaviour
 {
-    int patata;
-    // Start is called before the first frame update
-    void Start()
+    public GameObject hitboxUltimatePunch;
+    public GameObject objetoLanzable;
+    public GameObject objetoBumerang;
+
+    void Ejecutar()
     {
-        
+        if (SpritesManager.personajeJugadorUno == 1 || SpritesManager.personajeJugadorUno == 2 || SpritesManager.personajeJugadorUno == 9)
+        {
+            LanzarObjeto();
+        }
+
+        else if (SpritesManager.personajeJugadorUno == 3 || SpritesManager.personajeJugadorUno == 7)
+        {
+            LanzarBumerang();
+        }
+
+        else
+        {
+            Golpear();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void LanzarObjeto()
     {
-        
+        Instantiate(objetoLanzable, this.transform.position, Quaternion.Euler(this.transform.right));
+    }
+
+    void LanzarBumerang()
+    {
+        Instantiate(objetoBumerang, this.transform.position, Quaternion.Euler(this.transform.right));
+    }
+
+    void Golpear()
+    {
+        hitboxUltimatePunch.SetActive(true);
+        Invoke("DejarDeGolpear", 0.5f);
+    }
+
+    void DejarDeGolpear()
+    {
+        hitboxUltimatePunch.SetActive(false);
     }
 }
